850dd8a9474fece5b16de286e1f42a5f7044f102
Hojoong Jang <hojoong310@gmail.com>
2025-01-08 17:27:59 +0900

    feat: deploy scripts

---
+++ src/deploy/deploy_front.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd gst-front
+
+git pull origin feature/web-front
+echo "pull success"
+
+cd src/frontend
+
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+echo "source success"
+
+npm install
+echo "package install success"
+
+npm run build
+echo "static build success"
