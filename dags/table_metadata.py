b1fc88e35637505fa174f5d8a733b1575e25e977
최범준 <cbbsjj0314@gmail.com>
2025-01-10 12:29:10 +0900

    feat: add DAG to copy previous day's data from S3 to Redshift - 전날 데이터를 S3에서 Redshift로 적재 - table 메타데이터 파일 작성

---
+++ dags/table_metadata.py
@@ -0,0 +1,83 @@
+TABLES = {
+    'steam': [
+        {
+            'category': 'players',
+            'interval': '4-hourly',
+            'table_name': 'steam_players',
+            'staging_schema': """
+                CREATE TABLE IF NOT EXISTS silver.steam_players_staging (
+                    app_id VARCHAR(255),
+                    player_count BIGINT,
+                    result BIGINT,
+                    collected_at TIMESTAMP
+                );
+            """,
+            'columns': ['app_id', 'player_count', 'result', 'collected_at'],
+            's3_path': 'steam/players',
+            'join_condition': 's.app_id = t.app_id AND s.collected_at = t.collected_at',
+            'unique_val': 'app_id'
+        },
+        {
+            'category': 'review_metas',
+            'interval': 'daily',
+            'table_name': 'steam_review_metas',
+            'staging_schema': """
+                CREATE TABLE IF NOT EXISTS silver.steam_review_metas_staging (
+                    app_id VARCHAR(255),
+                    review_score BIGINT,
+                    review_score_desc VARCHAR(255),
+                    total_positive BIGINT,
+                    total_negative BIGINT,
+                    total_reviews BIGINT,
+                    collected_at TIMESTAMP
+                );
+            """,
+            'columns': ['app_id', 'review_score', 'review_score_desc', 'total_positive', 'total_negative', 'total_reviews', 'collected_at'],
+            's3_path': 'steam/review_metas',
+            'join_condition': 's.app_id = t.app_id AND s.collected_at = t.collected_at',
+            'unique_val': 'app_id',
+        },
+    ],
+    'twitch': [
+        {
+            'category': 'streams',
+            'interval': '4-hourly',
+            'table_name': 'twitch_streams',
+            'staging_schema': """
+                CREATE TABLE IF NOT EXISTS silver.twitch_streams_staging (
+                    id VARCHAR(255),
+                    user_name VARCHAR(255),
+                    game_id VARCHAR(255),
+                    game_name VARCHAR(255),
+                    type VARCHAR(255),
+                    title VARCHAR(255),
+                    viewer_count BIGINT,
+                    language VARCHAR(255),
+                    is_mature boolean,
+                    collected_at TIMESTAMP
+                );
+            """,
+            'columns': ['id', 'user_name', 'game_id', 'game_name', 'type', 'title', 'viewer_count', 'language', 'is_mature', 'collected_at'],
+            's3_path': 'twitch/streams',
+            'join_condition': 's.id = t.id AND s.collected_at = t.collected_at',
+            'unique_val': 'id'
+        },
+        {
+            'category': 'top_categories',
+            'interval': '4-hourly',
+            'table_name': 'twitch_top_categories',
+            'staging_schema': """
+                CREATE TABLE IF NOT EXISTS silver.twitch_top_categories_staging (
+                    id VARCHAR(255),
+                    name VARCHAR(255),
+                    igdb_id VARCHAR(255),
+                    collected_at TIMESTAMP
+                );
+            """,
+            'columns': ['id', 'name', 'igdb_id', 'collected_at'],
+            's3_path': 'twitch/top_categories',
+            'join_condition': 's.id = t.id AND s.collected_at = t.collected_at',
+            'unique_val': 'id',
+        },
+    ]
+}
