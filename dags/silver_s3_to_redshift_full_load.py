845b70b94e2bad4f5bb5736813c72434dd44274e
최범준 <cbbsjj0314@gmail.com>
2025-01-11 04:48:54 +0900

    feature: add DAG s3_to_redshift full load version - START_DATE와 END_DATE 사이의 데이터를 Redshift로 적재 - PK를 비교해서 이미 있는 데이터일 경우에는 COPY 스킵

--- dags/silver_s3_to_redshift_full_load.py
+++ dags/silver_s3_to_redshift_full_load.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from table_metadata import TABLES
 
 START_DATE = datetime(2024, 12, 30)
-END_DATE = datetime(2025, 1, 10)
+END_DATE = datetime(2025, 1, 11)
 
 REDSHIFT_SILVER_SCHEMA = Variable.get('REDSHIFT_SILVER_SCHEMA')
 REDSHIFT_IAM_ROLE = Variable.get('REDSHIFT_IAM_ROLE')
@@ -29,91 +29,103 @@ default_args = {
     'email': ['cbbsjj0314@gmail.com'],
 }
 
-def generate_valid_s3_paths():
-    logging.info("Generating valid S3 paths...")
+def generate_valid_s3_paths(source, category, interval):
+    logging.info(f"Generating valid S3 paths for {category}...")
     s3_hook = S3Hook(aws_conn_id='aws_gureum')
     valid_paths = []
     current_date = START_DATE
 
     while current_date <= END_DATE:
-        for hour in range(24):
-            s3_path = f"{S3_SILVER_BASE_PATH}/steam/players/{current_date.strftime('%Y-%m-%d')}/{hour:02d}/"
+        if interval == '4-hourly':
+            for hour in range(24):
+                s3_path = f"{S3_SILVER_BASE_PATH}/{source}/{category}/{current_date.strftime('%Y-%m-%d')}/{hour:02d}/"
+                objects = s3_hook.list_keys(bucket_name=S3_BUCKET_NAME, prefix=s3_path)
+                parquet_files = [obj for obj in objects if obj.endswith('.parquet')] if objects else []
+                if parquet_files:
+                    valid_paths.append(s3_path)
+        elif interval == 'daily':
+            s3_path = f"{S3_SILVER_BASE_PATH}/{source}/{category}/{current_date.strftime('%Y-%m-%d')}/"
             objects = s3_hook.list_keys(bucket_name=S3_BUCKET_NAME, prefix=s3_path)
             parquet_files = [obj for obj in objects if obj.endswith('.parquet')] if objects else []
             if parquet_files:
                 valid_paths.append(s3_path)
+        
         current_date += timedelta(days=1)
-    logging.info(f"Valid S3 paths: {valid_paths}")
+
+    logging.info(f"Valid S3 paths for {category}: {valid_paths}")
     return valid_paths
 
-s3_paths = generate_valid_s3_paths()
-
 with DAG(
-    dag_id='steam_players_s3_to_redshift_full_load',
+    dag_id='silver_s3_to_redshift_full_load',
     default_args=default_args,
-    description='Load processed silver data into Redshift using SQLExecuteQueryOperator',
+    description='START_DATE ~ END_DATE 사이의 S3에 적재된 Silver Layer를 Redshift로 COPY',
     schedule_interval=None,
     catchup=False,
     tags=['steam', 'silver'],
 ) as dag:
 
-    drop_staging_table = SQLExecuteQueryOperator(
-        task_id='drop_staging_table',
-        conn_id='redshift-gureum',
-        sql=f"DROP TABLE IF EXISTS {REDSHIFT_SILVER_SCHEMA}.{STAGING_TABLE_NAME};",
-    )
-    create_staging_table = SQLExecuteQueryOperator(
-        task_id='create_staging_table',
-        conn_id='redshift-gureum',
-        sql=f"""
-            CREATE TABLE IF NOT EXISTS {REDSHIFT_SILVER_SCHEMA}.{STAGING_TABLE_NAME} (
-                app_id VARCHAR(255),
-                player_count BIGINT,
-                result BIGINT,
-                collected_at TIMESTAMP
-            );
-        """,
-    )
+    for source, tables in TABLES.items():
+        print(f"Processing source: {source}")
+        for table in tables:
+            category = table['category']
+            interval = table['interval']
+            table_name = table['table_name']
+            staging_table_schema = table['staging_schema']
+            columns = table['columns']
+            join_condition = table['join_condition']
+            unique_val = table['unique_val']
 
-    copy_tasks = []
-    
-    for s3_path in s3_paths:
-        copy_to_staging = SQLExecuteQueryOperator(
-            task_id=f'copy_to_staging_{s3_path.split("/")[-2]}_{s3_path.split("/")[-1]}',
-            conn_id='redshift-gureum',
-            sql=f"""
-                COPY {REDSHIFT_SILVER_SCHEMA}.{STAGING_TABLE_NAME} (app_id, player_count, result, collected_at)
-                FROM 's3://{S3_BUCKET_NAME}/{s3_path}'
-                CREDENTIALS 'aws_iam_role={REDSHIFT_IAM_ROLE}'
-                FORMAT AS PARQUET;
-            """,
-        )
-        drop_staging_table >> create_staging_table >> copy_to_staging
-        copy_tasks.append(copy_to_staging)
+            s3_paths = generate_valid_s3_paths(source, category, interval)
 
-    merge_to_main_table = SQLExecuteQueryOperator(
-        task_id='merge_to_main_table',
-        conn_id='redshift-gureum',
-        sql=f"""
-            INSERT INTO {REDSHIFT_SILVER_SCHEMA}.{TABLE_NAME} (app_id, player_count, result, collected_at)
-            SELECT s.app_id, s.player_count, s.result, s.collected_at
-            FROM {REDSHIFT_SILVER_SCHEMA}.{STAGING_TABLE_NAME} s
-            LEFT JOIN {REDSHIFT_SILVER_SCHEMA}.{TABLE_NAME} t
-            ON s.app_id = t.app_id AND s.collected_at = t.collected_at
-            WHERE t.app_id IS NULL;
-        """,
-    )
+            drop_staging_table = SQLExecuteQueryOperator(
+                task_id=f'drop_staging_table_{table_name}',
+                conn_id='redshift-gureum',
+                sql=f"DROP TABLE IF EXISTS {REDSHIFT_SILVER_SCHEMA}.{table_name}_staging;",
+            )
 
-    for copy_task in copy_tasks:
-        copy_task >> merge_to_main_table
+            create_staging_table = SQLExecuteQueryOperator(
+                task_id=f'create_staging_table_{table_name}',
+                conn_id='redshift-gureum',
+                sql=staging_table_schema,
+            )
 
-    update_ingested_at = SQLExecuteQueryOperator(
-        task_id='update_ingested_at',
-        conn_id='redshift-gureum',
-        sql=f"""
-            UPDATE {REDSHIFT_SILVER_SCHEMA}.{TABLE_NAME}
-            SET ingested_at = timezone('Asia/Seoul', current_timestamp)
-            WHERE ingested_at IS NULL;
-        """,
-    )
-    merge_to_main_table >> update_ingested_at
+            copy_tasks = [] 
+            for s3_path in s3_paths:
+                copy_to_staging = SQLExecuteQueryOperator(
+                    task_id=f'copy_to_{table_name}_staging_{s3_path.split("/")[-2]}_{s3_path.split("/")[-1]}',
+                    conn_id='redshift-gureum',
+                    sql=f"""
+                        COPY {REDSHIFT_SILVER_SCHEMA}.{table_name}_staging ({', '.join(columns)})
+                        FROM 's3://{S3_BUCKET_NAME}/{s3_path}'
+                        IAM_ROLE '{REDSHIFT_IAM_ROLE}'
+                        FORMAT AS PARQUET;
+                    """,
+                )
+                drop_staging_table >> create_staging_table >> copy_to_staging
+                copy_tasks.append(copy_to_staging)
+
+            merge_to_main_table = SQLExecuteQueryOperator(
+                task_id=f'merge_to_main_table_{table_name}',
+                conn_id='redshift-gureum',
+                sql=f"""
+                    INSERT INTO {REDSHIFT_SILVER_SCHEMA}.{table_name} ({', '.join(columns)})
+                    SELECT {', '.join([f's.{col}' for col in columns])}
+                    FROM {REDSHIFT_SILVER_SCHEMA}.{table_name}_staging s
+                    LEFT JOIN {REDSHIFT_SILVER_SCHEMA}.{table_name} t
+                    ON {join_condition}
+                    WHERE t.{unique_val} IS NULL;
+                """,
+            )
+
+            update_ingested_at = SQLExecuteQueryOperator(
+                task_id=f'update_ingested_at_{table_name}',
+                conn_id='redshift-gureum',
+                sql=f"""
+                    UPDATE {REDSHIFT_SILVER_SCHEMA}.{table_name}
+                    SET ingested_at = timezone('Asia/Seoul', current_timestamp)
+                    WHERE ingested_at IS NULL;
+                """,
+            )
+            for copy_task in copy_tasks:
+                copy_task >> merge_to_main_table
+            merge_to_main_table >> update_ingested_at
