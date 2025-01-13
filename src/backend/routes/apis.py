2451c9ee6cd5b8561b5ef0d1d336998594f7c7c5
Hojoong Jang <hojoong310@gmail.com>
2025-01-13 13:10:42 +0900

    feat: add overviews

--- src/backend/routes/apis.py
+++ src/backend/routes/apis.py
@@ -12,18 +12,57 @@ async def get_test():
     return {'result': result}
 
 
-@router.get('/api/steam')
-async def get_steam():
+@router.get('/api/steam/overview')
+async def get_steam_summary():
+    query = 'SELECT * FROM gold.steam_overview'
+    result = db.execute_query(query)
+    return {'result': result}
+
+
+@router.get('/api/steam/popular')
+async def get_steam_popular():
     query = 'SELECT * FROM gold.steam_popular_games_v2'
     result = db.execute_query(query)
     return {'result': result}
 
 
-@router.get('/api/youtube')
-async def get_youtube():
-    pass
+@router.get('/api/steam/recommend')
+async def get_steam_recommend():
+    query = 'SELECT * FROM gold.recommend_steam_top_games_v2'
+    result = db.execute_query(query)
+    return {'result': result}
 
 
-@router.get('/api/twitch')
-async def get_youtube():
-    pass
+@router.get('/api/youtube/overview')
+async def get_youtube_overview():
+    query = 'SELECT * FROM gold.youtube_overview'
+    result = db.execute_query(query)
+    return {'result': result}
+
+
+@router.get('/api/youtube/view')
+async def get_youtube_like():
+    query = 'SELECT * FROM gold.youtube_view_videosv2 ORDER BY current_collected_at DESC, current_view_count DESC;'
+    result = db.execute_query(query)
+    return {'result': result}
+
+
+@router.get('/api/youtube/like')
+async def get_youtube_view():
+    query = 'SELECT * FROM gold.youtube_like_count_trendv2 ORDER BY current_collected_at DESC, current_view_count DESC;'
+    result = db.execute_query(query)
+    return {'result': result}
+
+
+@router.get('/api/twitch/overview')
+async def get_twitch_overview():
+    query = 'SELECT * FROM gold.twtich_overview'
+    result = db.execute_query(query)
+    return {'result': result}
+
+
+@router.get('/api/twitch/game')
+async def get_twitch_game():
+    query = 'SELECT * FROM gold.twitch_game_summary_v2 LIMIT 100'
+    result = db.execute_query(query)
+    return {'result': result}
